1e8937b9c47aff15aabde812556af1ce35ed0a2d
R0GUD <105155300+R0GUD@users.noreply.github.com>
2023-01-21 15:12:18 +0300

    Add files via upload

---
+++ src/components/StudentInfo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{ student.name }}
+        <img v-bind:src="student.photo">
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        props:{
+            id: '',
+        },
+        data() {
+            return {
+                student: {},
+            };
+        },
+        mounted: function(){
+            axios.get('http://34.82.81.113:3000/students/'+this.id).then((response) => {
+                console.log(response.data);
+                this.student = response.data;
+            });
+        },  
+    }
+</script>
